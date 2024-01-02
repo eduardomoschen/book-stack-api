2fdd72dfb06deeb9b47d81a74a6dd8faf67c43f0
Eduardo Moschen <eduardo.moschen28@gmail.com>
2024-01-02 10:04:54 -0300

    adicionando um campo para marcar a disponibilidade do livro

--- books/models.py
+++ books/models.py
@@ -14,6 +14,7 @@ class Book(models.Model):
     year_of_publication = YearField()
     publisher = models.CharField(max_length=255, blank=True, null=True)
     image = models.URLField()
+    available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
