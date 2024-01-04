87995f0484277ff1e7539ba9137efa211b51feb8
Eduardo Moschen <eduardo.moschen28@gmail.com>
2024-01-04 13:53:40 -0300

    aumento do tempo de expiração do token para fins de testes

--- app/settings.py
+++ app/settings.py
@@ -108,7 +108,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "BLACKLIST_AFTER_ROTATION": False,
     "SIGNING_KEY": os.environ.get('SECRET_KEY_JWT', 'INSECURE'),
