915cd6d584d9f3c458dfd6c16e132be8d783e297
Eduardo Moschen <eduardo.moschen28@gmail.com>
2024-01-05 16:47:27 -0300

    feat: adição do model para notificar o usuário
    esta notificação é feita através da área administrativa do Django, ou quando tiver alguma interface, informando o nome do livro e que tem apenas mais um dia para a devolução do mesmo

--- library/models.py
+++ library/models.py
@@ -12,6 +12,17 @@ class BookBorrowing(models.Model):
     date_issued = models.DateField()
     due_date = models.DateField()
     returned = models.BooleanField(default=False)
+    date_delivered = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.book} - {self.borrower}"
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.message}'
