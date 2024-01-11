6e5af046a0adfbdf75cc58ff5dca6b41983abc32
Eduardo Moschen <eduardo.moschen28@gmail.com>
2024-01-11 13:11:46 -0300

    :books: docs: adicionando a documentação das urls

--- books/urls.py
+++ books/urls.py
@@ -1,3 +1,19 @@
+"""
+Arquivo de configuração de URLs para o aplicativo de livros.
+
+Módulos:
+    - path: Módulo utilizado para definir padrões de URL.
+    - views: Módulo contendo as visualizações relacionadas ao aplicativo de
+    livros.
+
+Padrões de URL:
+    - books/: Rota para a visualização que lida com a listagem e criação de
+    livros.
+    - book/<int:isbn>/: Rota para a visualização que lida com os detalhes de um
+    livro específico.
+
+"""
+
 from django.urls import path
 from books import views
 
