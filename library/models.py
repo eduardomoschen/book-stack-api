fe3894a787a6c1476f61b2bf7d92672a28cc9280
Eduardo Moschen <eduardo.moschen28@gmail.com>
2024-01-16 16:54:01 -0300

    :books: docs: adicionando a documentação dos models

--- library/models.py
+++ library/models.py
@@ -7,6 +7,22 @@ User = get_user_model()
 
 
 class BookBorrowing(models.Model):
+    """
+    Reprentação do model para o empréstimo do livro.
+
+    Atributos:
+        - book: O livro que foi emprestado.
+        - borrower: O usuário que realizou o empréstimo.
+        - date_issued: A data que foi realizada o empréstimo.
+        - due_date: A data que deverá ser devolvido o livro.
+        - returned: Campo booleano que representa a disponibilidade do livro.
+        - date_delivered: A data que o livro foi entregue.
+
+    Métodos:
+        __str__: Retorna uma representação em string do título do livro e do
+        usuário que realizou o empréstimo.
+    """
+
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     borrower = models.ForeignKey(User, on_delete=models.CASCADE)
     date_issued = models.DateField()
@@ -19,6 +35,21 @@ class BookBorrowing(models.Model):
 
 
 class Notification(models.Model):
+    """
+    Representação da notificação que será enviada ao usuário a respeito das
+    datas dos livros.
+
+    Atributos:
+        - user: O usuário a ser notificado.
+        - message: A mensagem de acordo com a finalidade desejada.
+        - created_at: O dia em que foi realizada a notificação para o usuário.
+        - read: Campo booleano para informar se a mensagem foi lida ou não.
+
+    Métodos:
+        __str__: Retorna uma representação em string informando o username e a
+        mensagem enviada.
+    """
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
