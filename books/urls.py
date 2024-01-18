76ffdb543729508dc9baed06dd38f501a424dfe9
Eduardo Moschen <eduardo.moschen28@gmail.com>
2024-01-18 13:40:52 -0300

    :boom: docs(fix): corrigindo a grafia

--- books/urls.py
+++ books/urls.py
@@ -3,15 +3,12 @@ Arquivo de configuração de URLs para o aplicativo de livros.
 
 Módulos:
     - path: Módulo utilizado para definir padrões de URL.
-    - views: Módulo contendo as visualizações relacionadas ao aplicativo de
-    livros.
+    - views: Módulo contendo as views relacionadas ao aplicativo de livros.
 
 Padrões de URL:
-    - books/: Rota para a visualização que lida com a listagem e criação de
-    livros.
-    - book/<int:isbn>/: Rota para a visualização que lida com os detalhes de um
-    livro específico.
-
+    - books/: Rota para a view que lida com a listagem e criação de livros.
+    - book/<int:isbn>/: Rota para a view que lida com os detalhes de um livro
+    específico.
 """
 
 from django.urls import path
