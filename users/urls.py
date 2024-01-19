bf5165da077eeed7440e4b1a00dd4f477bdb11fa
Eduardo Moschen <eduardo.moschen28@gmail.com>
2024-01-19 15:24:35 -0300

    :books: docs: adicionando a documentação das urls

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,17 @@
+"""
+Arquivo de configuração de URLs para a app de users.
+
+Módulos:
+    - path: Módulo utilizado para definir padrões de URL.
+    - views: Módulo contendo as views relacionadas a app de users.
+
+Padrões de URL:
+    - users/: Rota para a view que lida com a listagem e criação de contas
+    para os usuários.
+    - user/<str:username>/: Rota para a view que lida com os detalhes de uma
+    conta de usuário específica.
+"""
+
 from django.urls import path
 from users import views
 
