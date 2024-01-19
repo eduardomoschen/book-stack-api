0152823be258a904ebe76682d73f5e7f46f9b635
Eduardo Moschen <eduardo.moschen28@gmail.com>
2024-01-19 15:24:23 -0300

    :books: docs: adicionando a documentação dos serializers

--- users/serializers.py
+++ users/serializers.py
@@ -3,6 +3,28 @@ from django.contrib.auth.hashers import make_password
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer para o model User.
+
+    Atributos:
+        - Esta classe não possui atributos.
+
+    Métodos:
+        - create: Cria um novo usuário com base nos dados validados.
+
+    Campos:
+        - id: Primary Key do usuário.
+        - first_name: Primeiro nome do usuário.
+        - last_name: Último nome do usuário
+        - username: Nome de usuário escolhido pelo usuário.
+        - email: E-mail do usuário.
+        - phone_number: Número de telefone do usuário.
+        - birth_date: Data de nascimento do usuário.
+        - is_active: Indica se a conta do usuário está ativa.
+        - is_staff: Indica se o usuário tem privilégios de staff.
+        - is_superuser: Indica se o usuário tem privilégios de superuser.
+    """
+
     class Meta:
         model = User
         fields = (
@@ -19,6 +41,16 @@ class UserSerializer(serializers.ModelSerializer):
         )
 
     def create(self, validated_data):
+        """
+        Cria um novo usuário com base nos dados validados.
+
+        Parâmetros:
+            - validated_data: Dados validados para criação do usuário.
+
+        Retorna:
+            - User: Instância do usuário criado.
+        """
+
         password = validated_data.pop('password', None)
         user = User(**validated_data)
 
